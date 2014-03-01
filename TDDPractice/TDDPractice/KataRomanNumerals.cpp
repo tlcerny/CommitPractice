270f99d433f0f64310eda6d7eab29d834f41d79e
Tlcerny <thomascerny@gmail.com>
2014-02-28 19:36:37 -0800

    Test 4 fails

--- TDDPractice/TDDPractice/KataRomanNumerals.cpp
+++ TDDPractice/TDDPractice/KataRomanNumerals.cpp
@@ -16,10 +16,12 @@ string RomanNumerals(int number) {
 		return "IV";
 
 	}
+	
 }
 
 TEST_CASE("Factorials are computed", "[factorial]") {
 	REQUIRE(RomanNumerals(1) == "I");
 	REQUIRE(RomanNumerals(2) == "II");
 	REQUIRE(RomanNumerals(4) == "IV");
+	REQUIRE(RomanNumerals(5) == "V");
 }
